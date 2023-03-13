c8b3328167722c4c9c453c93bd9168f48ea0e6ab
fcyen <chingyenf@gmail.com>
2023-03-13 09:02:39 +0800

    Open e-p tab if already existing

--- src/background.js
+++ src/background.js
@@ -2,5 +2,15 @@
  * When the extension icon is clicked, opens a new tab and displays the main page
  */
 chrome.action.onClicked.addListener(() => {
-    chrome.tabs.create({url: '../extension/index.html'});
+    const queryInfo = {
+        title: 'Elastic Pomodoro'
+    };
+    chrome.tabs.query(queryInfo)
+        .then((tabs) => {
+            if (tabs.length === 0) {
+                chrome.tabs.create({url: '../extension/index.html'})
+            } else {
+                chrome.tabs.update(tabs[0].id, { active: true })
+            }
+        });
 });
